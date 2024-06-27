503f3a570057b0b7548c8337e079d3cd7ea4ee7b
pedrohihi <polarp993@gmail.com>
2024-06-27 17:41:33 -0300

    cria bolinha 27/06/2024

--- pinpong.js
+++ pinpong.js
@@ -1,3 +1,8 @@
+//Aqui vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tambolinha = 25;
+-
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -5,6 +10,13 @@ function setup(){
 
 
 function draw(){
-    //aqui vou por cor da "'AWWDADADSQWDADWAIYVPOJCmesa"
+    //aqui vou por cor da "mesa"
     background(148,0,211);
+
+    criaBolinha(xBolinha, yBolinha, tambolinha)
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha, tambolinha){
+    circle (xBolinha,yBolinha,tambolinha);
 }
